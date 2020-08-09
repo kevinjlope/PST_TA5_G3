2b42ac653f87b210ff00d4b93fb747d354fcdd1d
Jodezamb <67130967+Jodezamb@users.noreply.github.com>
2020-08-09 01:13:24 -0500

    > Delay para visualizar el splash screen dentro del MainActivity, cabe recalcar que este delay no es recomendable implemetarlo, solo se lo uso para visualizacion.
    > Creacion de la SplashActivity  para mostrar la pantalla de bienvenida.
    
    >Creacion del mismo estilo entre el MainActivity y el SplashActivity.
    
    > Se anadio el icono representativo de la empresa.

--- app/src/main/java/com/example/pst_g3/MainActivity.java
+++ app/src/main/java/com/example/pst_g3/MainActivity.java
@@ -8,6 +8,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        setTheme(R.style.AppTheme); //indicarle a nuestro main activity va a tener un nuevo tema
+        //que es encargado de mostra el splashscreen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
