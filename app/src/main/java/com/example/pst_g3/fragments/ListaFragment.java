5f0241d550397d030a3b29990dbda84e0c15d515
Sergio_Ramos <serleram@espol.edu.ec>
2020-08-14 21:04:59 -0500

    Commit Sergio

--- app/src/main/java/com/example/pst_g3/fragments/ListaFragment.java
+++ app/src/main/java/com/example/pst_g3/fragments/ListaFragment.java
@@ -55,6 +55,11 @@ public class ListaFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+    }
+
+    public void sinopsis (View view){
+        System.out.println("HOLA AMOR");
     }
 
     @Override
